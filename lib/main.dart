8440847426f4e4f52453110b86e44b86be0aff8a
M-Amin <mohammedcps@gmail.com>
2023-10-25 16:06:41 +0300

    Test2R

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'blablabla testing app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Welcome please go away'),
     );
   }
 }
@@ -46,20 +46,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
+            Text('testing success ^^)', style: TextStyle(fontWeight: FontWeight.bold,),),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
